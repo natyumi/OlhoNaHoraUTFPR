e3f1e68cefbfe64610dc272a44ba97939699815a
natyumi <natalia.yumi.cam@gmail.com>
2025-01-26 19:02:44 -0300

    🔨: add delete activity

---
+++ src/home/DeleteActivityModal.tsx
@@ -0,0 +1,69 @@
+import { IoMdClose } from "react-icons/io";
+import { Activities } from "./Home";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, database } from "../firebase";
+import { increment, ref, remove, update } from "firebase/database";
+
+interface ICreateActivityModal {
+  open: boolean;
+  onClose: () => void;
+  activity: Activities;
+  fetchActivities: (userId: string) => void;
+}
+export default function DeleteActivityModal({
+  open,
+  onClose,
+  activity,
+  fetchActivities,
+}: ICreateActivityModal) {
+
+  function deleteActivity() {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        remove(
+          ref(
+            database,
+            `activities/${user.uid}/${activity?.group}/${activity?.id}`
+          )
+        );
+        update(ref(database, `activities/${user.uid}/${activity?.group}/points`), {
+          points: increment(-activity?.points),
+        })
+        fetchActivities(user.uid);
+        onClose();
+      }
+    });
+  }
+  return (
+    <dialog className={`modal ${open && "modal-open"} animate-ping`}>
+      <div className="modal-box p-8 max-w-fit grid gap-8">
+        <div className="flex flex-row items-center justify-between">
+          <p className="font-bold text-lg">Deseja excluir a atividade?</p>
+          <button
+            className="btn btn-ghost btn-circle btn-secondary btn-sm hover:bg-gray-200"
+            onClick={onClose}
+          >
+            <IoMdClose size={16} />
+          </button>
+        </div>
+        <p>Essa ação excluirá a atividade permanentemente</p>
+        <div>
+          <div className="flex flex-row justify-evenly gap-10">
+            <button
+              className="btn btn-sm disabled:bg-gray-200 btn-wide"
+              onClick={onClose}
+            >
+              Cancelar
+            </button>
+            <button
+              className="btn btn-error btn-sm disabled:bg-gray-200 w-52"
+              onClick={deleteActivity}
+            >
+              Excluir
+            </button>
+          </div>
+        </div>
+      </div>
+    </dialog>
+  );
+}
--- src/home/components/ActivitiesTable.tsx
+++ src/home/components/ActivitiesTable.tsx
@@ -1,18 +1,46 @@
-import { Activities } from "../Home";
+import { Activities } from '../Home'
+import { LuDownload } from 'react-icons/lu'
+import { MdModeEditOutline } from 'react-icons/md'
+import { FiTrash2 } from 'react-icons/fi'
+import { useState } from 'react'
+import DeleteActivityModal from '../DeleteActivityModal'
 
 interface IActivityTable {
   activities: Activities[]
+  fetchActivities: (userId: string) => void
 }
 
-export default function ActivityTable({ activities }: IActivityTable) {
+export default function ActivityTable({
+  activities,
+  fetchActivities,
+}: IActivityTable) {
+  const [openDeleteActivityModal, setOpenDeleteActivityModal] =
+    useState<boolean>(false)
+  const [activity, setActivity] = useState<Activities>({
+    name: '',
+    group: '',
+    points: 0,
+    description: '',
+    start: '',
+    end: '',
+    duration: '',
+    image: '',
+    id: '',
+    imageBuffer: new ArrayBuffer(0),
+  })
+
   return (
     <div>
       {activities.length > 0 ? (
         <table className="table">
-          <thead className=' bg-secondary-100'>
+          <thead className=" bg-secondary-100">
             <tr>
               <th>Nome</th>
+              <th>Descrição</th>
               <th>Pontos</th>
+              <th>Início</th>
+              <th>Término</th>
+              <th>Duração</th>
               <th>Certificado</th>
             </tr>
           </thead>
@@ -21,8 +49,43 @@ export default function ActivityTable({ activities }: IActivityTable) {
               return (
                 <tr key={index}>
                   <td>{activity.name}</td>
+                  <td>{activity.description}</td>
                   <td>{activity.points}</td>
-                  <td>{activity.image}</td>
+                  <td>{activity.start}</td>
+                  <td>{activity.end}</td>
+                  <td>{activity.duration}</td>
+                  <td>
+                    <a
+                      href={activity.image}
+                      target="_blank"
+                      className="flex flex-row items-center gap-2 tooltip tooltip-top"
+                      data-tip="Baixar arquivo"
+                    >
+                      <button className="btn btn-xs btn-primary">
+                        <LuDownload />
+                      </button>
+                    </a>
+                  </td>
+                  <td>
+                    <div className="flex flex-row items-center gap-6">
+                      <button
+                        className="tooltip btn btn-ghost btn-sm p-0"
+                        data-tip="Editar atividade"
+                      >
+                        <MdModeEditOutline />
+                      </button>
+                      <button
+                        className="tooltip btn btn-ghost btn-sm p-0"
+                        data-tip="Excluir atividade"
+                        onClick={() => {
+                          setOpenDeleteActivityModal(true)
+                          setActivity(activity)
+                        }}
+                      >
+                        <FiTrash2 className="text-red-500" />
+                      </button>
+                    </div>
+                  </td>
                 </tr>
               )
             })}
@@ -31,6 +94,12 @@ export default function ActivityTable({ activities }: IActivityTable) {
       ) : (
         <p>Você ainda não possui atividades nessa categoria</p>
       )}
+      <DeleteActivityModal
+        open={openDeleteActivityModal}
+        onClose={() => setOpenDeleteActivityModal(false)}
+        activity={activity}
+        fetchActivities={fetchActivities}
+      />
     </div>
-  );
+  )
 }
